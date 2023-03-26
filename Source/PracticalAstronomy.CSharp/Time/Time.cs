a707fda707dd1ddd94d9277d1b952ca72cc4cfbe
Mateusz Pawlikowski <30951232+Pawleshhh@users.noreply.github.com>
2023-03-26 13:40:09 +0200

    GstToLst and LstToGst C#

--- Source/PracticalAstronomy.CSharp/Time/Time.cs
+++ Source/PracticalAstronomy.CSharp/Time/Time.cs
@@ -10,4 +10,10 @@ public class Time
     public TimeSpan GstToUniversalTime(DateTime dateTime)
         => PracticalAstronomy.Time.gstToUt(dateTime);
 
+    public TimeSpan GstToLst(double longitude, TimeSpan gst)
+        => PracticalAstronomy.Time.gstToLst(longitude, gst);
+
+    public TimeSpan LstToGst(double longitude, TimeSpan lst)
+        => PracticalAstronomy.Time.lstToGst(longitude, lst);
+
 }
