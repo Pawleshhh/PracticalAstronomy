5ad105d7868643eb21388147fd561a6f846f0eb4
Mateusz Pawlikowski <30951232+Pawleshhh@users.noreply.github.com>
2023-03-31 13:46:47 +0200

    CSharp: RightAscensionToHourAngle; HourAngleToRightAscension; EquatorialToHorizon

---
+++ Source/PracticalAstronomy.CSharp/Coordinates/CoordinateSystemTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace PracticalAstronomy.CSharp;
+
+public record Coord2D(double X, double Y)
+{
+    public static Coord2D FromTuple(Tuple<double, double> tuple)
+        => new Coord2D(tuple.Item1, tuple.Item2);
+}
+
+public record Coord3D(double X, double Y, double Z);
---
+++ Source/PracticalAstronomy.CSharp/Coordinates/Coordinates.cs
@@ -0,0 +1,18 @@
+﻿namespace PracticalAstronomy.CSharp;
+
+public class Coordinates
+{
+
+    private Coordinates() { }
+
+    public double RightAscensionToHourAngle(DateTime dateTime, double longitude, double rightAscension)
+        => PracticalAstronomy.Coordinates.raToHa(dateTime, longitude, rightAscension);
+
+    public double HourAngleToRightAscension(DateTime dateTime, double longitude, double hourAngle)
+        => PracticalAstronomy.Coordinates.haToRa(dateTime, longitude, hourAngle);
+
+    public Coord2D EquatorialToHorizon(Coord2D equatorial, double latitude)
+        => Coord2D.FromTuple(
+            PracticalAstronomy.Coordinates.equatorialToHorizon(equatorial.X, equatorial.Y, latitude));
+
+}
