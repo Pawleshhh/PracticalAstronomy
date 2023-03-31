a358031eb09874712bee6fa6d5f58dbb134ad6b8
Mateusz Pawlikowski <30951232+Pawleshhh@users.noreply.github.com>
2023-03-31 12:44:44 +0200

    haToRa

--- Source/PracticalAstronomy.Test/Coordinates/CoordinatesTest.fs
+++ Source/PracticalAstronomy.Test/Coordinates/CoordinatesTest.fs
@@ -9,3 +9,9 @@ let ``Test raToHa`` (ra : float) (longitude : float) (year : int) (month : int) 
     let dateTime = (new DateTime(year, month, day)).AddHours(time)
     let result = raToHa dateTime longitude ra
     Assert.That(result, Is.EqualTo(ha).Within(1E-6))
+
+[<TestCase(9.873_239, -64.0, 1980, 4, 22, 18.614_353, 18.539_165)>]
+let ``Test haToRa`` (ha : float) (longitude : float) (year : int) (month : int) (day : int) (time : float) (ra : float) =
+    let dateTime = (new DateTime(year, month, day)).AddHours(time)
+    let result = haToRa dateTime longitude ha
+    Assert.That(result, Is.EqualTo(ra).Within(1E-6))
--- Source/PracticalAstronomy/Coordinates/Coordinates.fs
+++ Source/PracticalAstronomy/Coordinates/Coordinates.fs
@@ -9,3 +9,10 @@ let raToHa dateTime longitude ra =
     |> gstToLst longitude
     |> fun lst -> lst.TotalHours - ra
     |> fun h1 -> if h1 < 0.0 then h1 + 24.0 else h1
+
+let haToRa dateTime longitude ha =
+    dateTime
+    |> dateTimeToGst
+    |> gstToLst longitude
+    |> fun lst -> lst.TotalHours - ha
+    |> fun ra -> if ra < 0.0 then ra + 24.0 else ra
