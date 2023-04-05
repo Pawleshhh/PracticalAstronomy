a3d58098ccade17d90130f9ef7f2e679fe9fe9af
Mateusz Pawlikowski <30951232+Pawleshhh@users.noreply.github.com>
2023-04-05 20:31:01 +0200

    Generalised transformation unit tests final part

--- Source/PracticalAstronomy.Test/Coordinates/CoordinatesTest.fs
+++ Source/PracticalAstronomy.Test/Coordinates/CoordinatesTest.fs
@@ -5,10 +5,10 @@ open NUnit.Framework
 open PracticalAstronomy.Coordinates
 open PracticalAstronomy.CoordinateSystemTypes
 
-[<TestCase(97.638_119, -17.857_969, 23.446_236, 5.155_862, 52.175_278, 153.491_944, 40.399_444)>]
-let ``generalisedTransformation EclToHor`` (lon: float) (lat: float) (mo: float) (st: float) (geoLat: float) (az: float) (alt: float) =
-    let result = generalisedTransformation (EclToHor(mo, TimeSpan.FromHours(st), geoLat)) (lon, lat)
-    Assert.That(result, Is.EqualTo((az, alt)).Within(1E-5))
+//[<TestCase(97.638_119, -17.857_969, 23.446_236, 5.155_862, 52.175_278, 153.491_944, 40.399_444)>]
+//let ``generalisedTransformation EclToHor`` (lon: float) (lat: float) (mo: float) (st: float) (geoLat: float) (az: float) (alt: float) =
+//    let result = generalisedTransformation (EclToHor(mo, TimeSpan.FromHours(st), geoLat)) (lon, lat)
+//    Assert.That(result, Is.EqualTo((az, alt)).Within(1E-5))
 
 [<TestCase(147.558_5835, 42.124_235, 4.412_4043, 278.627_481)>]
 let ``generalisedTransformation HaToEq`` (ha: float) (dec: float) (st: float) (ra: float) =
@@ -28,6 +28,86 @@ let ``generalisedTransformation HaToEcl`` (ha: float) (dec: float) (st: float) (
 [<TestCase(147.558_5835, 42.124_235, 4.412_4043, 70.788_4839, 20.504_4140)>]
 let ``generalisedTransformation HaToGal`` (ha: float) (dec: float) (st: float) (lon: float) (lat: float) =
     let result = generalisedTransformation (HaToGal(TimeSpan.FromHours(st))) (ha, dec)
+    Assert.That(result, Is.EqualTo((lon, lat)).Within(1E-5))
+
+[<TestCase(278.627_481, 42.124_235, 4.412_4043, 147.558_5835)>]
+let ``generalisedTransformation EqToHa`` (ra: float) (dec: float) (st: float) (ha: float) =
+    let result = generalisedTransformation (EqToHa(TimeSpan.FromHours(st))) (ra, dec)
+    Assert.That(result, Is.EqualTo((ha, dec)).Within(1E-6))
+
+[<TestCase(278.627_481, 42.124_235, 4.412_4043, -64.0, 235.9807995, -61.3127810)>]
+let ``generalisedTransformation EqToHor`` (ra: float) (dec: float) (st: float) (lat: float) (az: float) (alt: float) =
+    let result = generalisedTransformation (EqToHor(TimeSpan.FromHours(st), lat)) (ra, dec)
+    Assert.That(result, Is.EqualTo((az, alt)).Within(1E-6))
+
+[<TestCase(278.627_481, 42.124_235, 23.439_2318, 285.325_7326, 65.105_1288)>] //Date: 2009/07/06
+let ``generalisedTransformation EqToEcl`` (ra: float) (dec: float) (mo: float) (lon: float) (lat: float) =
+    let result = generalisedTransformation (EqToEcl(mo)) (ra, dec)
+    Assert.That(result, Is.EqualTo((lon, lat)).Within(1E-6))
+
+[<TestCase(278.627_481, 42.124_235, 70.788_4839, 20.504_4140)>]
+let ``generalisedTransformation EqToGal`` (ra: float) (dec: float) (lon: float) (lat: float) =
+    let result = generalisedTransformation (EqToGal) (ra, dec)
+    Assert.That(result, Is.EqualTo((lon, lat)).Within(1E-5))
+    
+[<TestCase(235.9807995, -61.3127810, -64.0, 147.558_5835, 42.124_235)>]
+let ``generalisedTransformation HorToHa`` (az: float) (alt: float) (lat: float) (ha: float) (dec: float) =
+    let result = generalisedTransformation (HorToHa(lat)) (az, alt)
+    Assert.That(result, Is.EqualTo((ha, dec)).Within(1E-6))
+
+[<TestCase(235.9807995, -61.3127810, 4.412_4043, -64.0, 278.627_481, 42.124_235)>]
+let ``generalisedTransformation HorToEq`` (az: float) (alt: float) (st: float) (lat: float) (ra: float) (dec: float) =
+    let result = generalisedTransformation (HorToEq(TimeSpan.FromHours(st), lat)) (az, alt)
+    Assert.That(result, Is.EqualTo((ra, dec)).Within(1E-6))
+
+[<TestCase(235.9807995, -61.3127810, 4.412_4043, -64.0, 23.439_2318, 285.325_7326, 65.105_1288)>] //Date: 2009/07/06
+let ``generalisedTransformation HorToEcl`` (az: float) (alt: float) (st: float) (geoLat: float) (mo: float) (lon: float) (lat: float) =
+    let result = generalisedTransformation (HorToEcl(TimeSpan.FromHours(st), geoLat, mo)) (az, alt)
+    Assert.That(result, Is.EqualTo((lon, lat)).Within(1E-6))
+
+[<TestCase(235.9807995, -61.3127810, 4.412_4043, -64.0, 70.788_4839, 20.504_4140)>]
+let ``generalisedTransformation HorToGal`` (az: float) (alt: float) (st: float) (geoLat: float) (lon: float) (lat: float) =
+    let result = generalisedTransformation (HorToGal(TimeSpan.FromHours(st), geoLat)) (az, alt)
+    Assert.That(result, Is.EqualTo((lon, lat)).Within(1E-5))
+
+[<TestCase(285.325_7326, 65.105_1288, 23.439_2318, 278.627_481, 42.124_235)>] //Date: 2009/07/06
+let ``generalisedTransformation EclToEq`` (lon: float) (lat: float) (mo: float) (ra: float) (dec: float) =
+    let result = generalisedTransformation (EclToEq(mo)) (lon, lat)
+    Assert.That(result, Is.EqualTo((ra, dec)).Within(1E-6))
+
+[<TestCase(285.325_7326, 65.105_1288, 23.439_2318, 4.412_4043, 147.558_5835, 42.124_235)>] //Date: 2009/07/06
+let ``generalisedTransformation EclToHa`` (lon: float) (lat: float) (mo: float) (st: float) (ha: float) (dec: float) =
+    let result = generalisedTransformation (EclToHa(mo, TimeSpan.FromHours(st))) (lon, lat)
+    Assert.That(result, Is.EqualTo((ha, dec)).Within(1E-6))
+
+[<TestCase(285.325_7326, 65.105_1288, 23.439_2318, 4.412_4043, -64.0, 235.9807995, -61.3127810)>] //Date: 2009/07/06
+let ``generalisedTransformation EclToHor`` (lon: float) (lat: float) (mo: float) (st: float) (geoLat: float) (az: float) (alt: float) =
+    let result = generalisedTransformation (EclToHor(mo, TimeSpan.FromHours(st), geoLat)) (lon, lat)
+    Assert.That(result, Is.EqualTo((az, alt)).Within(1E-6))
+
+[<TestCase(285.325_7326, 65.105_1288, 23.439_2318, 70.788_4839, 20.504_4140)>] //Date: 2009/07/06
+let ``generalisedTransformation EclToGal`` (lon: float) (lat: float) (mo: float) (galLon: float) (galLat: float) =
+    let result = generalisedTransformation (EclToGal(mo)) (lon, lat)
+    Assert.That(result, Is.EqualTo((galLon, galLat)).Within(1E-5))
+
+[<TestCase(70.788_4839, 20.504_4140, 278.627_481, 42.124_235)>]
+let ``generalisedTransformation GalToEq`` (lon: float) (lat: float) (ra: float) (dec: float) =
+    let result = generalisedTransformation (GalToEq) (lon, lat)
+    Assert.That(result, Is.EqualTo((ra, dec)).Within(1E-5))
+
+[<TestCase(70.788_4839, 20.504_4140, 4.412_4043, 147.558_5835, 42.124_235)>]
+let ``generalisedTransformation GalToHa`` (lon: float) (lat: float)  (st: float) (ha: float) (dec: float) =
+    let result = generalisedTransformation (GalToHa(TimeSpan.FromHours(st))) (lon, lat)
+    Assert.That(result, Is.EqualTo((ha, dec)).Within(1E-5))
+
+[<TestCase(70.788_4839, 20.504_4140, 4.412_4043, -64.0, 235.9807995, -61.3127810)>]
+let ``generalisedTransformation GalToHor`` (lon: float) (lat: float) (st: float) (geoLat: float) (az: float) (alt: float)  =
+    let result = generalisedTransformation (GalToHor(TimeSpan.FromHours(st), geoLat)) (lon, lat)
+    Assert.That(result, Is.EqualTo((az, alt)).Within(1E-5))
+
+[<TestCase(70.788_4839, 20.504_4140, 23.439_2318, 285.325_7326, 65.105_1288)>] //Date: 2009/07/06
+let ``generalisedTransformation GalToEcl`` (galLon: float) (galLat: float) (mo: float) (lon: float) (lat: float) =
+    let result = generalisedTransformation (GalToEcl(mo)) (galLon, galLat)
     Assert.That(result, Is.EqualTo((lon, lat)).Within(1E-5))
 
 [<TestCase(18.539_167, -64.0, 1980, 4, 22, 18.614_353, 9.873_237)>]
