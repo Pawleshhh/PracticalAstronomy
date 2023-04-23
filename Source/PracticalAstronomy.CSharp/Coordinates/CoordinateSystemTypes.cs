ff679efba2366d2c532f4270a4dac9fe16b3f1c0
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2023-04-23 13:35:42 +0200

    C# support for generalised transfomrations

--- Source/PracticalAstronomy.CSharp/Coordinates/CoordinateSystemTypes.cs
+++ Source/PracticalAstronomy.CSharp/Coordinates/CoordinateSystemTypes.cs
@@ -1,5 +1,7 @@
 ﻿namespace PracticalAstronomy.CSharp;
 
+using FConversion = CoordinateSystemTypes.CoordConversion;
+
 public record Coord2D(double X, double Y)
 {
     public static Coord2D FromTuple(Tuple<double, double> tuple)
@@ -8,27 +10,87 @@ public record Coord2D(double X, double Y)
 
 public record Coord3D(double X, double Y, double Z);
 
-public abstract record CoordConversion
-{
-    //internal abstract PracticalAstronomy.CoordinateSystemTypes.CoordConversion ToFSharp();
+public abstract record CoordConversion{
+    internal abstract FConversion ToFSharp();
 }
-public record HaToEq(TimeSpan SiderealTime) : CoordConversion;
-public record HaToHor(double Latitude) : CoordConversion;
-public record HaToEcl(TimeSpan SiderealTime, double MeanObliquity) : CoordConversion;
-public record HaToGal(TimeSpan SiderealTime) : CoordConversion;
-public record EqToHa(TimeSpan SiderealTime) : CoordConversion;
-public record EqToHor(TimeSpan SiderealTime, double Latitude) : CoordConversion;
-public record EqToEcl(double MeanObliquity) : CoordConversion;
-public record EqToGal : CoordConversion;
-public record HorToHa(double Latitude) : CoordConversion;
-public record HorToEq(TimeSpan SiderealTime, double Latitude) : CoordConversion;
-public record HorToEcl(TimeSpan SiderealTime, double Latitude, double MeanObliquity) : CoordConversion;
-public record HorToGal(TimeSpan SiderealTime, double Latitude) : CoordConversion;
-public record EclToEq(double MeanObliquity) : CoordConversion;
-public record EclToHa(double MeanObliquity, TimeSpan SiderealTime) : CoordConversion;
-public record EclToHor(double MeanObliquity, TimeSpan SiderealTime, double Latitude) : CoordConversion;
-public record EclToGal(double MeanObliquity) : CoordConversion;
-public record GalToEq : CoordConversion;
-public record GalToHa(TimeSpan SiderealTime) : CoordConversion;
-public record GalToHor(TimeSpan SiderealTime, double Latitude) : CoordConversion;
-public record GalToEcl(double MeanObliquity) : CoordConversion;
+public record HaToEq(TimeSpan SiderealTime) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewHaToEq(SiderealTime);
+}
+
+public record HaToHor(double Latitude) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewHaToHor(Latitude);
+}
+public record HaToEcl(TimeSpan SiderealTime, double MeanObliquity) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewHaToEcl(SiderealTime, MeanObliquity);
+}
+public record HaToGal(TimeSpan SiderealTime) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewHaToGal(SiderealTime);
+}
+public record EqToHa(TimeSpan SiderealTime) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewEqToHa(SiderealTime);
+}
+public record EqToHor(TimeSpan SiderealTime, double Latitude) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewEqToHor(SiderealTime, Latitude);
+}
+public record EqToEcl(double MeanObliquity) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewEqToEcl(MeanObliquity);
+}
+public record EqToGal : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.EqToGal;
+}
+public record HorToHa(double Latitude) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewHorToHa(Latitude);
+}
+public record HorToEq(TimeSpan SiderealTime, double Latitude) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewHorToEq(SiderealTime, Latitude);
+}
+public record HorToEcl(TimeSpan SiderealTime, double Latitude, double MeanObliquity) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewHorToEcl(SiderealTime, Latitude, MeanObliquity);
+}
+public record HorToGal(TimeSpan SiderealTime, double Latitude) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewHorToGal(SiderealTime, Latitude);
+}
+public record EclToEq(double MeanObliquity) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewEclToEq(MeanObliquity);
+}
+public record EclToHa(double MeanObliquity, TimeSpan SiderealTime) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewEclToHa(MeanObliquity, SiderealTime);
+}
+public record EclToHor(double MeanObliquity, TimeSpan SiderealTime, double Latitude) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewEclToHor(MeanObliquity, SiderealTime, Latitude);
+}
+public record EclToGal(double MeanObliquity) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewEclToGal(MeanObliquity);
+}
+public record GalToEq : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.GalToEq;
+}
+public record GalToHa(TimeSpan SiderealTime) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewGalToHa(SiderealTime);
+}
+public record GalToHor(TimeSpan SiderealTime, double Latitude) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewGalToHor(SiderealTime, Latitude);
+}
+public record GalToEcl(double MeanObliquity) : CoordConversion{
+    internal override FConversion ToFSharp()
+        => FConversion.NewGalToEcl(MeanObliquity);
+}
--- Source/PracticalAstronomy.CSharp/Coordinates/Coordinates.cs
+++ Source/PracticalAstronomy.CSharp/Coordinates/Coordinates.cs
@@ -1,38 +1,40 @@
 ﻿namespace PracticalAstronomy.CSharp;
 
+using FCoordinates = PracticalAstronomy.Coordinates;
+
 public class Coordinates
 {
 
     private Coordinates() { }
 
     public Coord2D GeneralisedTransformation(CoordConversion conversion, Coord2D coord)
-        => PracticalAstronomy.Coordinates.generalisedTransformation(conversion.ToFSharp(), coord.X, coord.Y);
+        => FCoordinates.generalisedTransformation(conversion.ToFSharp(), coord.X, coord.Y).ToCoord2D();
 
     public double RightAscensionToHourAngle(DateTime dateTime, double longitude, double rightAscension)
-        => PracticalAstronomy.Coordinates.raToHa(dateTime, longitude, rightAscension);
+        => FCoordinates.raToHa(dateTime, longitude, rightAscension);
 
     public double HourAngleToRightAscension(DateTime dateTime, double longitude, double hourAngle)
-        => PracticalAstronomy.Coordinates.haToRa(dateTime, longitude, hourAngle);
+        => FCoordinates.haToRa(dateTime, longitude, hourAngle);
 
     public Coord2D EquatorialToHorizontal(Coord2D equatorial, double latitude)
-        => PracticalAstronomy.Coordinates.equatorialToHorizontal(latitude, equatorial.X, equatorial.Y).ToCoord2D();
+        => FCoordinates.equatorialToHorizontal(latitude, equatorial.X, equatorial.Y).ToCoord2D();
 
     public Coord2D HorizontalToEquatorial(Coord2D horizontal, double latitude)
-        => PracticalAstronomy.Coordinates.horizontalToEquatorial(latitude, horizontal.X, horizontal.Y).ToCoord2D();
+        => FCoordinates.horizontalToEquatorial(latitude, horizontal.X, horizontal.Y).ToCoord2D();
 
     public double MeanObliquity(DateTime dateTime)
-        => PracticalAstronomy.Coordinates.meanObliquity(dateTime);
+        => FCoordinates.meanObliquity(dateTime);
 
     public Coord2D EclipticToEquatorial(Coord2D ecliptic, DateTime dateTime)
-        => PracticalAstronomy.Coordinates.eclitpicToEquatorial(dateTime, ecliptic.X, ecliptic.Y).ToCoord2D();
+        => FCoordinates.eclitpicToEquatorial(dateTime, ecliptic.X, ecliptic.Y).ToCoord2D();
 
     public Coord2D EquatorialToEcliptic(Coord2D equatorial, DateTime dateTime)
-        => PracticalAstronomy.Coordinates.equatorialToEcliptic(dateTime, equatorial.X, equatorial.Y).ToCoord2D();
+        => FCoordinates.equatorialToEcliptic(dateTime, equatorial.X, equatorial.Y).ToCoord2D();
 
     public Coord2D EquatorialToGalactic(Coord2D equatorial)
-        => PracticalAstronomy.Coordinates.equatorialToGalactic(equatorial.X, equatorial.Y).ToCoord2D();
+        => FCoordinates.equatorialToGalactic(equatorial.X, equatorial.Y).ToCoord2D();
 
     public Coord2D GalacticToEquatorial(Coord2D galactic)
-        => PracticalAstronomy.Coordinates.galacticToEquatorial(galactic.X, galactic.Y).ToCoord2D();
+        => FCoordinates.galacticToEquatorial(galactic.X, galactic.Y).ToCoord2D();
 
 }
