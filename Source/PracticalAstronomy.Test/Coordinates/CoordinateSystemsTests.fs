53916267941bdd20b2ad662b464fae535676288b
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2023-05-20 13:16:02 +0200

    haToRa

--- Source/PracticalAstronomy.Test/Coordinates/CoordinateSystemsTests.fs
+++ Source/PracticalAstronomy.Test/Coordinates/CoordinateSystemsTests.fs
@@ -10,3 +10,10 @@ open PracticalAstronomy.Test.TestUtils
 let raToHa y m d t lon ra ha =
     let result = raToHa (createDateTimeWithDecimalHours y m d t) lon ra
     Assert.That(result, Is.EqualTo(ha).Within(1E-5))
+
+[<TestCase(1980, 4, 22, 18.614_353,  -64.0, 148.098_555, 278.087_505)>]
+[<TestCase(2034, 1, 31,  4.006_172, -120.0,  70.799_010, 359.749_995)>]
+[<TestCase(2011, 4, 10, 15.060_897,    1.0, 344.230_827,  81.179_166)>]
+let haToRa y m d t lon ha ra =
+    let result = haToRa (createDateTimeWithDecimalHours y m d t) lon ha
+    Assert.That(result, Is.EqualTo(ra).Within(1E-5))
--- Source/PracticalAstronomy/Coordinates/CoordinateSystems.fs
+++ Source/PracticalAstronomy/Coordinates/CoordinateSystems.fs
@@ -2,7 +2,6 @@
 
 open System
 open PracticalAstronomy.Units
-open PracticalAstronomy.MathHelper
 open PracticalAstronomy.Time
 
 let raToHa (dateTime: DateTime) (longitude: float<deg>) (ra: float<deg>) =
@@ -11,4 +10,12 @@ let raToHa (dateTime: DateTime) (longitude: float<deg>) (ra: float<deg>) =
     |> gstToLst longitude
     |> fun lst -> lst.TotalHours - (ra / 15.0<deg>)
     |> fun h1 -> if h1 < 0.0 then h1 + 24.0 else h1
+    |> (*) 15.0<deg>
+
+let haToRa (dateTime: DateTime) (longitude: float<deg>) (ha: float<deg>) =
+    dateTime
+    |> dateTimeToGst
+    |> gstToLst longitude
+    |> fun lst -> lst.TotalHours - (ha / 15.0<deg>)
+    |> fun ra -> if ra < 0.0 then ra + 24.0 else ra
     |> (*) 15.0<deg>
