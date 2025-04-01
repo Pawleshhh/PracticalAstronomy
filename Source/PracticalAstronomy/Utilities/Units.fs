49e514d8bdeff9763a6304bab11c52e68d0d3209
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2025-04-01 20:14:56 +0200

    Refraction implemented and covered in unit tests

--- Source/PracticalAstronomy/Utilities/Units.fs
+++ Source/PracticalAstronomy/Utilities/Units.fs
@@ -1,5 +1,12 @@
 ﻿module PracticalAstronomy.Units
 
+// Coordinates
 [<Measure>] type deg
 [<Measure>] type rad
 [<Measure>] type arcsec
+
+// Temperatures
+[<Measure>] type celsius
+
+// Atmospheric pressure
+[<Measure>] type mbar
